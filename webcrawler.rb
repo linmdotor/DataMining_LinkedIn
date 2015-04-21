ecd0e37bafa8738d1ad5e423f52093667ec6b9f7
William Hedén <pwheden@gmail.com>
2015-04-21 19:10:48 +0200

    Total change of method
    It is illegal to crawl with a LinkedIn account, and it didn't work well for me, so what I do now is:
    
    1. Search on Google for "experience [company name] LinkedIn"
    2. Find all links that look like "[something] | LinkedIn" scrolling through 10 google pages
    3. Enter all these links and see if the person has [company name] under experience, and in that case, save all his/her skills with his/her name.
    
    This method works, but you don't get that many people in the end. It's very sensitive to the country you input, as a search for a swedish company will be a lot better on swedish google than on the spanish google. Therefore I made country an input as well as company!
    
    A big help would be to create a function that, once on a profile, clicks on profiles from "People Also Viewed" cause these are usually people from the same company. But this will work for now. 
    
    A search with good hits is:
    Company: Kidbrooke Advisory
    Country: Sweden
    (If you want to see how it works)
    
    NOTE: Requires the file NameSkills too.

--- webcrawler.rb
+++ webcrawler.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 #Testing building a web crawler
 #If Mechanize proves difficult installing, it's because there's a bug with Command Line Tools for Xcode 6.3
 #Download for 6.2 @ https://developer.apple.com/downloads/index.action#
@@ -8,44 +10,66 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
+require_relative 'NameSkills'
+
 #Initialize Mechanize
 agent = Mechanize.new
 agent.user_agent_alias = "Mac Safari"
 agent.follow_meta_refresh = true
 
-#Sign in to LinkedIn
-page = agent.get('https://www.linkedin.com/uas/login')
-login_form = page.form('login')
-login_form.session_key = 'bigdataminingproject@gmail.com'
-login_form.session_password = 'bigdataminingproject123'
-page = agent.submit(login_form, login_form.buttons[0])
-
 #What company are you interested in?
-Insert_Company = "Kidbrooke Advisory"
+puts "What company are you interested in? "
+Insert_Company = gets.chomp
 
-#Do a Google search of "LinkedIn 'Insert_Company' Employees"
-page = agent.get('https://www.google.com')
+#Where do most of the company employees reside?
+puts "Where do most of the company employees reside? "
+country = gets.chomp
+
+if country.casecmp("Sweden") == 0 #0 is true
+	prefix = "se"
+elsif country.casecmp("Norway") == 0
+	prefix = "no"
+elsif country.casecmp("Spain") == 0
+	prefix = "es"
+else	
+	prefix = "com"
+end
+
+#Do a Google search of "experience 'Insert_Company' LinkedIn"
+page = agent.get("https://www.google.com/ncr")
+page = agent.get("https://www.google.#{prefix}")
 search_form = page.form('f')
-search_form.q = "LinkedIn #{Insert_Company} Employees"
+search_form.q = "experience #{Insert_Company} LinkedIn"
 page = agent.submit(search_form, search_form.buttons[0])
 
-#Click on the link named "'Insert_Company' | LinkedIn"
-page = agent.page.links_with(:text => "#{Insert_Company} | LinkedIn")[0].click
+#Find all links with "Something | LinkedIn" and put them the names in L, and the links in M
+i = 2
+L = []
+M = []
+while i<9 do
+	page.links.each do |link|
+  		if link.text.include? "| LinkedIn"
+  			L.push(link.text.slice(0..(link.text.index('|')-1)))
+  			#M.push(URI.extract("#{link.uri()}"))
+  			M.push("https://www.google.#{prefix}#{link.uri}")
+  		end
+	end
+#Scroll through the first 10 Google search pages and continue filling the list L and M.
+	page = agent.page.links_with(:text => "#{i}")[0].click
+	i += 1
+end
 
-#Current URL
-next_page = agent.page.uri.to_s
+#Click each link in M, see if the person has experience from 'Insert_Company' and in that case, save their skills in a list N with their name.
+N = Hash.new
+for i in 0..(M.length-1)
+	puts "#{(((i+1)*100)/M.length.to_f).round(1)}% completed. Please hold on while we crunch the numbers."
+ 	hash_name, hash_skills = NameSkills(Insert_Company, "#{M[i]}")
+ 		if (hash_name.empty? == false && hash_skills.empty? == false)
+ 			N[hash_name] = hash_skills
+ 		end
+end
 
-#Find the link that redirects to company employees within the JS code
-results = agent.get(next_page).body
-s = StringScanner.new(results)
-s = s.scan_until /See all/
-s = s.lines.last
-a = (0 ... s.length).find_all { |i| s[i,1] == '"' }
-s = s[a[2]+1..a[3]-1]
-next_page = "https://www.linkedin.com/#{s}"
-page = agent.get(next_page)
-
-#Find the names of the employees on the first page, later try to expand into going through all pages
-#CURRENT WORKING FUNCTION
-results = agent.get(next_page).body
-puts results
+N.each do |key, value|
+	puts "#{key}:#{value}"
+	puts
+end

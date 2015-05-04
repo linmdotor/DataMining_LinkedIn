58e12b43bf5233a9f3992cd3c0f67e9f663d57f3
Bianca Dufour Partanen <biancadufourpartanen@gmail.com>
2015-05-04 13:48:52 +0200

    added comments

--- AlsoViewed.rb
+++ AlsoViewed.rb
@@ -1,3 +1,6 @@
+# function for upgrading the web crawler: this funtion clicks on the "people also viewed" links on a certain
+# Linked in profile.
+
 def AlsoViewed(insert_company, urlpage)
 
 	require 'rubygems'
@@ -15,24 +18,24 @@ def AlsoViewed(insert_company, urlpage)
 	
 	L = []
 	Insert_Company = "Ericsson"
-	page = agent.get('https://se.linkedin.com/in/cristiannorlin')
+	page = agent.get('https://se.linkedin.com/in/cristiannorlin') # goes to the LinkedIn of a user
 
 	#page = agent.get("#{urlpage}")
 
 	page.links.each do |link|
-	  L.push(link.uri.to_s())
+	  L.push(link.uri.to_s()) #pushes all links on the page to a list
 	end
 	L = L.uniq
 
 	N = Hash.new
 	N[Insert_Company] = {}
 	for link in L 
-		if link.include? "pub"
+		if link.include? "pub" #takes only the links which will redirect to a new profile
 			#a,b = NameSkills( Insert_Company, link)
-			hash_name, hash_skills = NameSkills(Insert_Company, link)
+			hash_name, hash_skills = NameSkills(Insert_Company, link) #call the function Nameskills which checks if the desired company is among the experience
 			#puts a,b
 			if (hash_name.empty? == false && hash_skills.empty? == false)
- 				N[Insert_Company][hash_name] = {'Skills'=>hash_skills,'LinkedIn URL'=>link}
+ 				N[Insert_Company][hash_name] = {'Skills'=>hash_skills,'LinkedIn URL'=>link} #hashes the skills, the link to a dictionary, the name
  			end
 
 		end

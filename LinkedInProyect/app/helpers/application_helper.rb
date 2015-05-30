cf5f6dd5a02b0ce47e7528ecca3a33edf69fd9f5
William Hedén <pwheden@gmail.com>
2015-05-31 00:00:20 +0200

    Fixed a few errors

--- LinkedInProyect/app/helpers/application_helper.rb
+++ LinkedInProyect/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   #If no specific title is given on a certain page, the "base_title" is shown on top of the page instead.
   #Use this to provide a specific title in the 'views' scripts: <% provide(:title, "Home") %>
   def full_title(page_title = '')
-    base_title = "WhatSkills?"
+    base_title = "Mining LinkedIn"
     if page_title.empty?
       base_title
     else

f9861573a478fd18ea2c30e59b6de7a6b04d5bbf
Lazlo <larsalmg@gmail.com>
2015-06-01 11:39:32 +0200

    Fixed the bug that made everything crash if the crawler could not find any results to mine. The page now prints "No company found".

--- LinkedInProyect/app/controllers/similarities_controller.rb
+++ LinkedInProyect/app/controllers/similarities_controller.rb
@@ -26,35 +26,39 @@ class SimilaritiesController < ApplicationController
     comp = Company.where(:name => company.to_s).pluck(:_id)[0].to_s
     empl = User.where(:company_id => comp).pluck(:name, :skill_ids).to_a
 
-    skills = [];
-    occurences = [];
+    if empl.length > 0
+      skills = [];
+      occurences = [];
 
-    (0...(empl.length)).each do |i|
-      (0...(empl[i][1].to_a.length)).each do |j|
-        unless skills.include?(empl[i][1].to_a[j])
-          skills.push(empl[i][1].to_a[j]);
-          occurences.push(1);
-        else
-          occurences[skills.index(empl[i][1].to_a[j])] += 1;
+      (0...(empl.length)).each do |i|
+        (0...(empl[i][1].to_a.length)).each do |j|
+          unless skills.include?(empl[i][1].to_a[j])
+            skills.push(empl[i][1].to_a[j]);
+            occurences.push(1);
+          else
+            occurences[skills.index(empl[i][1].to_a[j])] += 1;
+          end
         end
       end
-    end
 
-    total = 0;
-    (0...(occurences.length)).each do |i|
-      total += occurences[i]
-    end
+      total = 0;
+      (0...(occurences.length)).each do |i|
+        total += occurences[i]
+      end
 
-    index = Array.new(skills.length);
-    (0...(index.length)).each do |i|
-      index[i] = [skills[i], occurences[i].to_f/total];
-    end
+      index = Array.new(skills.length);
+      (0...(index.length)).each do |i|
+        index[i] = [skills[i], occurences[i].to_f/total];
+      end
 
-    indexSort = index.sort_by{|s,n| n }.reverse;
-    @top3 = [];
+      indexSort = index.sort_by{|s,n| n }.reverse;
+      @top3 = [];
 
-    (0..2).each do |i|
-      @top3 << Skill.where(:_id => indexSort[i][0])
+      (0..2).each do |i|
+        @top3 << Skill.where(:_id => indexSort[i][0])
+      end
+    else
+      @top3 = {}
     end
 
   end
@@ -66,40 +70,43 @@ class SimilaritiesController < ApplicationController
     allEmp = User.where(:company_id => comp).pluck(:name, :skill_ids).to_a
     myUser = User.where(:_id => id).pluck(:skill_ids).to_a
 
-    countSkills = Array.new(allEmp.length) {0};
+    if allEmp.length > 0
 
-    myUser[0].each do |skill|
-      (0...(allEmp.length)).each do |i|
-        if allEmp[i][1].include?(skill)
-          countSkills[i] += 1;
+      countSkills = Array.new(allEmp.length) {0};
+
+      myUser[0].each do |skill|
+        (0...(allEmp.length)).each do |i|
+          if allEmp[i][1].include?(skill)
+            countSkills[i] += 1;
+          end
         end
       end
-    end
 
-    division = countSkills
+      division = countSkills
 
-    (0...(countSkills.length)).each do |i|
-      puts "***************"
-      puts allEmp[i][0]
-      puts countSkills[i] / (myUser[0].length + allEmp[i][1].length  - division[i]).to_f;
-      countSkills[i] /= (myUser[0].length + allEmp[i][1].length - division[i]).to_f;
-    end
-
-    puts countSkills.max
-
-    max = countSkills.max;
-    bestFit = [];
-    (0...(countSkills.length)).each do |i|
-      if countSkills[i] == max
-        bestFit.push(i)
+      (0...(countSkills.length)).each do |i|
+        puts "***************"
+        puts allEmp[i][0]
+        puts countSkills[i] / (myUser[0].length + allEmp[i][1].length  - division[i]).to_f;
+        countSkills[i] /= (myUser[0].length + allEmp[i][1].length - division[i]).to_f;
       end
-    end
 
-    @theBest = User.where(:name => allEmp[bestFit[0]][0])
+      puts countSkills.max
 
-    @theSkills = []
-    (0...(allEmp[bestFit[0]][1].length)).each do |i|
-      @theSkills << Skill.where(:_id => allEmp[bestFit[0]][1][i])
+      max = countSkills.max;
+      bestFit = [];
+      (0...(countSkills.length)).each do |i|
+        if countSkills[i] == max
+          bestFit.push(i)
+        end
+      end
+
+      @theBest = User.where(:name => allEmp[bestFit[0]][0])
+
+      @theSkills = []
+      (0...(allEmp[bestFit[0]][1].length)).each do |i|
+        @theSkills << Skill.where(:_id => allEmp[bestFit[0]][1][i])
+      end
     end
 
   end
